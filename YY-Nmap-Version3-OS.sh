71d3d45b14515a9ffda319debf8c6e77223a5e2b
ucki <ucki@toppoint.de>
2016-05-08 11:56:16 +0200

    Update YY-Nmap-Version3-OS.sh

--- YY-Nmap-Version3-OS.sh
+++ YY-Nmap-Version3-OS.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-echo Checking the OS and Version Intensity 2
+echo Checking the OS and Version Intensity 3
 for word in $(cat ./results/online-IP.txt); do nmap  -Pn -n -vv  -sTU -p "$(< ./results/$word/$word-totalPortlist-inline.txt)" --version-intensity 3 -O -oA ./results/$word/$word-nmapOScheck-VersionIntensity3  $word & done
 
 
