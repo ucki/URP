3c143b24ec9a1b0a716fa2b5f79bbfc2676cc733
ucki <ucki@toppoint.de>
2016-05-08 19:56:29 +0200

    Delete 9-Nmap-RegisteredPorts.sh

--- 9-Nmap-RegisteredPorts.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo Nmap Registered Ports
-for word in $(cat ./results/online-IP.txt);do nmap  -Pn -n -vv  -sTU -p1024-49151 --version-intensity 0 -oA ./results/$word/$word-nmapRegisteredPorts  $word & done
